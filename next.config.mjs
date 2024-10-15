26cf31646a42527519c5205184a0a773c02fd914
Multiprocesssing-CODER <sagnik.dey745@icloud.com>
2024-10-16 02:45:51 +0530

    Still testing for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    unoptimized: true, // Disable default image optimization
-  },
-  assetPrefix: isProd ? "/Code-Blog" : "",
-  basePath: isProd ? "/Code-Blog" : "",
-};
+const nextConfig = {};
 
 export default nextConfig;
