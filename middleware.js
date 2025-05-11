1719c59bc04c6fee3db60c8bc4cdd3a203bc3567
Multiprocesssing-CODER <sagnik.dey745@icloud.com>
2025-05-12 00:10:31 +0530

    Update public route matcher to include additional API routes for fetching blogs and comments

--- middleware.js
+++ middleware.js
@@ -1,6 +1,18 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)", "/", "/blogpost/(.*)"]);
+const isPublicRoute = createRouteMatcher([
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/",
+  "/blog",
+  "/api/fetch-blogs",
+  "/api/fetch-blogs/(.*)",
+  "/api/comments",
+  "/api/comments/(.*)",
+  "/blogpost/(.*)",
+  "/about",
+  "/about/(.*)",
+]);
 
 export default clerkMiddleware(async (auth, req) => {
   if (!isPublicRoute(req)) {
