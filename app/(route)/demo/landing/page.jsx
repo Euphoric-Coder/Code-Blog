aadf00cd78707937de5a1797f39c04847641a9d3
Multiprocesssing-CODER <sagnik.dey745@icloud.com>
2025-06-11 03:15:04 +0530

    Working new UI of landing page

---
+++ app/(route)/demo/landing/page.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Hero } from "./_components/Hero";
+import { Header } from "./_components/Header";
+
+const page = () => {
+  return (
+    <div>
+      <Header />
+      <Hero />
+    </div>
+  );
+};
+
+export default page;
